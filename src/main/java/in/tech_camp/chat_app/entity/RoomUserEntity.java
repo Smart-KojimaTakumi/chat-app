f238bbad04a74ae2ca7b1ce7c09099c7d7826735
Smart-KojimaTakumi <takumi.kojima@smart-group.co.jp>
2026-04-21 10:22:59 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/entity/RoomUserEntity.java
+++ src/main/java/in/tech_camp/chat_app/entity/RoomUserEntity.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class RoomUserEntity {
   private Integer id;
-  private UserEntity user_id;
-  private RoomEntity room_id;
+  private UserEntity user;
+  private RoomEntity room;
 }
