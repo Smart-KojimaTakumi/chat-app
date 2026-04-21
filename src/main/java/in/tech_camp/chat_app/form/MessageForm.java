f58509c153d0f10e18c359b1653cf32e76f03d41
Smart-KojimaTakumi <takumi.kojima@smart-group.co.jp>
2026-04-21 15:38:46 +0900

    画像保存の実装

--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -4,8 +4,12 @@ import in.tech_camp.chat_app.validation.ValidationPriority1;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import org.springframework.web.multipart.MultipartFile;
+
 @Data
 public class MessageForm {
   @NotBlank(message = "Name can't be blank",groups = ValidationPriority1.class)
   private String content;
+
+  private MultipartFile image;
 }
